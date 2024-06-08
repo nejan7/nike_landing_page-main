ef61426b1d10d70b4abcfb07847e2704a26475c9
nejan7 <nejanf.girl@gmail.com>
2024-06-09 01:08:44 +0330

    Subscribe section

--- client/src/sections/Subscribe.jsx
+++ client/src/sections/Subscribe.jsx
@@ -1,7 +1,34 @@
 const Subscribe = () => {
   return (
-    <div>Subscribe</div>
-  )
-}
+    <section
+      id="contact-us"
+      className="max-container flex justify-between items-center max-lg:flex-col gap-10"
+    >
+      <h3 className="text-4xl leading-[68px] lg:max-w-md font-palanquin font-bold">
+        Sign Up for
+        <span className="text-coral-red"> Updates </span>& Newsletter
+      </h3>
+      <div className="lg:max-w-[40%] w-full flex items-center max-sm:flex-col gap-5 p-2.5 sm:border sm:border-slate-gray rounded-full">
+        <input type="text" placeholder="subscribe@nike.com" className="input" />
+        <div className="flex max-sm:justify-end items-center max-sm:w-full">
+          {/* Start button =============*/}
+          <button
+            className=" overflow-hidden relative w-32 p-2 h-12 bg-coral-red
+            text-white border-none text-xl font-bold cursor-pointer z-10 group rounded-full"
+          >
+            Sing up
+            <span className="absolute w-36 h-32 -top-8 -left-2 bg-red-200 rounded-full transform scale-x-0 group-hover:scale-x-100 transition-transform group-hover:duration-500 duration-1000 origin-bottom"></span>
+            <span className="absolute w-36 h-32 -top-8 -left-2 bg-red-400 rounded-full transform scale-x-0 group-hover:scale-x-100 transition-transform group-hover:duration-700 duration-700 origin-bottom"></span>
+            <span className="absolute w-36 h-32 -top-8 -left-2 bg-red-600 rounded-full transform scale-x-0 group-hover:scale-x-100 transition-transform group-hover:duration-1000 duration-500 origin-bottom"></span>
+            <span className="group-hover:opacity-100 group-hover:duration-1000 duration-100 opacity-0 absolute top-3 left-6 z-10">
+              just click!
+            </span>
+          </button>
+          {/* End button =============*/}
+          </div>
+      </div>
+    </section>
+  );
+};
 
-export default Subscribe
+export default Subscribe;

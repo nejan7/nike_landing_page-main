7a665a68b9c2efd7d46858ff3a1c69a30dcd44ff
nejan7 <nejanf.girl@gmail.com>
2024-06-08 17:01:15 +0330

    Services component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -20,8 +20,7 @@ const App = ()=> (
     <section className="padding">
       <SuperQuality/>
     </section>
-    <section className="padding
-    py-10">
+    <section className="padding-x py-10">
       <Services/>
     </section>
     <section className="padding">
